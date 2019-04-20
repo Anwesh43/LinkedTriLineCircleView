8c2664d42e3c2e6cc759a22f5fac8ed27be35bc0
Anwesh43 <anweshthecool0@gmail.com>
2019-04-20 10:25:56 +0530

    adding TriLineCircleView class

--- trilinecircleview/src/main/java/com/anwesh/uiprojects/trilinecircleview/TriLineCircleView.kt
+++ trilinecircleview/src/main/java/com/anwesh/uiprojects/trilinecircleview/TriLineCircleView.kt
@@ -70,3 +70,21 @@ fun Canvas.drawTLCNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TriLineCircleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
