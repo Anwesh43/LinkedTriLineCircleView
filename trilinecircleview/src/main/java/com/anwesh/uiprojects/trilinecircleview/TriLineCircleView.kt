41cf845b87a683d8eb1ff28a3fe86c7f84676ba2
Anwesh43 <anweshthecool0@gmail.com>
2019-04-20 09:40:05 +0530

    adding constants and libraries

--- trilinecircleview/src/main/java/com/anwesh/uiprojects/trilinecircleview/TriLineCircleView.kt
+++ trilinecircleview/src/main/java/com/anwesh/uiprojects/trilinecircleview/TriLineCircleView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.trilinecircleview
 /**
  * Created by anweshmishra on 20/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
