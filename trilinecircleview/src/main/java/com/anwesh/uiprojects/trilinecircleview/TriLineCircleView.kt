2fc6febe2e47950c2fcecd5d65a47bad5a42be1e
Anwesh43 <anweshthecool0@gmail.com>
2019-04-20 10:22:01 +0530

    adding drawTLCNode canvas extension method

--- trilinecircleview/src/main/java/com/anwesh/uiprojects/trilinecircleview/TriLineCircleView.kt
+++ trilinecircleview/src/main/java/com/anwesh/uiprojects/trilinecircleview/TriLineCircleView.kt
@@ -21,6 +21,8 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 90f
+val triSizeFactor : Float = 0.66f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -31,3 +33,40 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawTriLineCircle(sc : Float, angleDeg : Float, size : Float, paint : Paint) {
+    val deg : Double = angleDeg * 0.5f * Math.PI / 180
+    val r : Float = size * triSizeFactor
+    val x : Float = r * Math.sin(deg).toFloat()
+    val y : Float = r * Math.cos(deg).toFloat()
+    val rc : Float = size - r
+    save()
+    translate(0f, y)
+    drawLine(-x, 0f, x, 0f, paint)
+    drawArc(RectF(-x, -rc, x, rc), 0f, 180f * sc, false, paint)
+    restore()
+}
+
+fun Canvas.drawTLCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    val angleBetweenLines : Float = 360f / lines
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(rotDeg * sc2)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(angleBetweenLines * j)
+        drawTriLineCircle(sc1.divideScale(j, lines), angleBetweenLines, size, paint)
+        restore()
+    }
+    restore()
+}
